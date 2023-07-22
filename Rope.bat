45a8f02040ddc7d96e5dc7d0636020fff9414ded
Hillobar <63615199+Hillobar@users.noreply.github.com>
2023-07-22 14:55:20 -0700

    Add files via upload

---
+++ Rope.bat
@@ -0,0 +1,3 @@
+call venv\Scripts\activate.bat
+python run_working.py 
+pause
---
+++ run_working.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+from rope import core_working
+if __name__ == "__main__":
+    core_working.run()
